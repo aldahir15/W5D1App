3ffe52b4a03b0f40ad724c45a0be0fbf1782c875
Aldahir <aldahir@Aldahirs-Macbook-Pro.local>
2017-08-28 14:03:06 -0700

    goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,28 @@
 class GoalsController < ApplicationController
+  before_action :require_login
+
+  def index
+    @goals = Goal.where('private_or_public = public') + Goal.find_by(user_id: current_user.id)
+    @goals = @goals.uniq
+    render :index
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    @goal.user_id = current_user.id
+    if @goal.save
+      redirect_to goals_link
+    else
+      flash[:errors] = @goal.errors.full_messages
+      render :new
+    end
+  end
+
+  def goal_params
+    params.require(:goal).permit(:goal_body, :progress, :private_or_public)
+  end
 end
--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -2,4 +2,38 @@ require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
 
+  describe "Require Login" do
+    it "should require a login" do
+      get :index
+      expect(response).to redirect_to(new_session_url)
+    end
+  end
+
+  describe "GET #index" do
+    it "should render :index template of your goals and other public ones" do
+      get :index
+      expect(response).to be_success
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET #new" do
+    it "should render :new template" do
+      get :new
+      expect(response).to be_success
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST #create" do
+    it "should redirect to new if invalid" do
+      post :create, params: { goal: { user_id: 1, goal_body: 'Finishing this project', private_or_public: 'Public', progress: -10  }}
+      expect(response).to render_template(:new)
+    end
+
+    it "should create a new goal if valid" do
+      post :create, params: { goal: { user_id: 1, goal_body: 'Finishing this project', private_or_public: 'Public', progress: 20  }}
+      expect(response).to redirect_to(goals_url)
+    end
+  end
 end
